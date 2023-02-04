bebf53c639a05c7a597ba15d79ad9507a3382e2e
Valentin Zaretsky <vzaretsky@gmail.com>
2023-02-04 17:20:23 +0200

    sum row counts

--- week_2_homework/etl_gcs_to_bq.py
+++ week_2_homework/etl_gcs_to_bq.py
@@ -10,17 +10,17 @@ def extract_from_gcs(color: str, year: int, month: int) -> Path:
     """Download trip data from GCS"""
     gcs_path = f"data/{color}/{color}_tripdata_{year}-{month:02}.parquet"
     gcs_block = GcsBucket.load("zoom-gcs")
-    gcs_block.get_directory(from_path=gcs_path, local_path=f"../data/")
-    return Path(f"../data/{gcs_path}")
+    gcs_block.get_directory(from_path=gcs_path, local_path=f"./data/")
+    return Path(f"./data/{gcs_path}")
 
 
 @task(log_prints=True)
 def transform(path: Path) -> pd.DataFrame:
     """Data cleaning example"""
     df = pd.read_parquet(path)
-    print(f"pre: missing passenger count: {df['passenger_count'].isna().sum()}")
-    df["passenger_count"].fillna(0, inplace=True)
-    print(f"post: missing passenger count: {df['passenger_count'].isna().sum()}")
+#    print(f"pre: missing passenger count: {df['passenger_count'].isna().sum()}")
+#   df["passenger_count"].fillna(0, inplace=True)
+#    print(f"post: missing passenger count: {df['passenger_count'].isna().sum()}")
     return df
 
 
@@ -40,16 +40,24 @@ def write_bq(df: pd.DataFrame) -> None:
 
 
 @flow()
-def etl_gcs_to_bq():
+def etl_gcs_to_bq(year: int, month: int, color: str) -> int:
     """Main ETL flow to load data into Big Query"""
-    color = "yellow"
-    year = 2021
-    month = 1
-
     path = extract_from_gcs(color, year, month)
     df = transform(path)
     write_bq(df)
+    return len(df)
+
+
+
+@flow(log_prints=True)
+def etl_parent_flow(
+    months: list[int] = [2, 3], year: int = 2019, color: str = "yellow"
+):
+    total_rows=0
+    for month in months:
+        total_rows += etl_gcs_to_bq(year, month, color)
+    print(f"Total rows: {total_rows}")
 
 
 if __name__ == "__main__":
-    etl_gcs_to_bq()
+    etl_parent_flow()
